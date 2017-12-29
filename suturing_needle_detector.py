0fea82f690125f17ebf0eff6ed08b37ee9e9fb8c
francesco <francesco.chichi@gmail.com>
2017-12-29 19:18:54 +0100

    i

--- suturing_needle_detector.py
+++ suturing_needle_detector.py
@@ -2,16 +2,13 @@ import numpy as np
 import argparse
 import cv2
 
-def nothing(x):
-  pass
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 args = vars(ap.parse_args())
 
 if args.get("video", None) is None:
   print("no video founded")
-  camera = 0;
+  camera = 0
 else:
   camera = cv2.VideoCapture(args["video"])
 
@@ -28,6 +25,38 @@ while camera != 0:
 
   frame_diff = cv2.absdiff(current_frame_gray, previous_frame_gray)
 
+  '''
+  ret, threshold_output = cv2.threshold(frame_diff, 127, 255, 0)
+  im2, contours, hierarchy = cv2.findContours(threshold_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+  
+  minEllipse = np.array(len(contours))
+
+  for i in range(len(contours)):
+    if len(contours[i]) > 5:
+      minEllipse[i] = cv2.fitEllipse(contours[i])
+  '''
+
+  # Draw contours + rotated rects + ellipses
+  #drawing = np.zeros(len(threshold_output), cv2.CV_8UC3)
+
+  '''for( int i = 0; i< contours.size(); i++ )
+     {
+       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+       // contour
+       drawContours( drawing, contours, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+       // ellipse
+       ellipse( drawing, minEllipse[i], color, 2, 8 );
+       // rotated rectangle
+       Point2f rect_points[4]; minRect[i].points( rect_points );
+       for( int j = 0; j < 4; j++ )
+          line( drawing, rect_points[j], rect_points[(j+1)%4], color, 1, 8 );
+     }
+  
+  cv2.drawContours(current_frame, contours, -1, (0, 255, 0), 3)
+  '''
+
+
   cv2.imshow('frame diff ', frame_diff)
   cv2.imshow('original frame ', current_frame)
 
@@ -35,7 +64,6 @@ while camera != 0:
     break
 
   previous_frame = current_frame.copy()
-
 
   grabbed, current_frame = camera.read()
 
